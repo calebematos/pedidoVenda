a01defd61cf692c123320cbf3bd3a1055c8118ec
Lucas Calebe <calebematos@gmail.com>
2018-11-04 19:19:26 -0200

    Adicionando bean validator

--- src/main/java/br/com/calebematos/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/br/com/calebematos/pedidovenda/controller/CadastroProdutoBean.java
@@ -5,13 +5,28 @@ import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import br.com.calebematos.pedidovenda.model.Produto;
+
 @Named
 @ViewScoped
 public class CadastroProdutoBean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	private Produto produto;
+	
+	public CadastroProdutoBean() {
+		produto = new Produto();
+	}
+	
 	public void salvar() {
-		throw new RuntimeException("Teste de exceção!");
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
 	}
 }
